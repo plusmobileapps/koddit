04249cd5c39a027234b983efa597dd48d381af75
Andrew Steinmetz <andrew@plusmobileapps.com>
2020-05-28 14:41:38 -0700

    fix proguard and add reddit feed adapter for the android view

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -23,10 +23,10 @@
 # https://github.com/Kotlin/kotlinx.serialization#androidjvm
 -keepattributes *Annotation*, InnerClasses
 -dontnote kotlinx.serialization.SerializationKt
--keep,includedescriptorclasses class com.yourcompany.yourpackage.**$$serializer { *; } # <-- change package name to your app's
--keepclassmembers class com.yourcompany.yourpackage.** { # <-- change package name to your app's
+-keep,includedescriptorclasses com.plusmobileapps.koddit.**$$serializer { *; } # <-- change package name to your app's
+-keepclassmembers class com.plusmobileapps.koddit.** { # <-- change package name to your app's
     *** Companion;
 }
--keepclasseswithmembers class com.yourcompany.yourpackage.** { # <-- change package name to your app's
+-keepclasseswithmembers class com.plusmobileapps.koddit.** { # <-- change package name to your app's
     kotlinx.serialization.KSerializer serializer(...);
 }
--- app/src/main/java/com/plusmobileapps/koddit/FirstFragment.kt
+++ app/src/main/java/com/plusmobileapps/koddit/FirstFragment.kt
@@ -7,15 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.plusmobileapps.sharedcode.FeedRepository
 import com.plusmobileapps.sharedcode.createApplicationScreenMessage
+import com.plusmobileapps.sharedcode.db.data.Post
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), RedditFeedItemListener {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -27,17 +31,52 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<TextView>(R.id.textview_first).text = createApplicationScreenMessage()
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.reddit_home_feed)
+        val adapter = RedditFeedAdapter(this)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+            this.adapter = adapter
         }
         FeedRepository().getDankMemes(
-            onSuccess = {
-                Log.d("FirstFragment", it.toString())
+            onSuccess = { posts ->
+                Log.d("FirstFragment", posts.toString())
+                adapter.submitList(posts.map(Post::toRedditFeedItem))
             },
             onError = {
                 Log.e("FirstFragment", it.toString())
             }
         )
     }
+
+    override fun onMoreOptionsClicked(id: String) {
+
+    }
+
+    override fun onPostClicked(id: String, imageview: ImageView) {
+    }
+
+    override fun onUpVoteClicked(id: String) {
+    }
+
+    override fun onDownVoteClicked(id: String) {
+    }
+
+    override fun onCommentClicked(id: String) {
+    }
+
+    override fun onShareButtonClicked(id: String) {
+    }
+}
+
+private fun Post.toRedditFeedItem(): RedditFeedItem {
+    return RedditFeedItem(
+        id = id,
+        title = title,
+        subreddit = subreddit_name_prefixed,
+        username = author_fullname,
+        timePosted = "12:00", //todo
+        subredditImageUrl = "",
+        karmaCount = ups.toString(),
+        description = url
+    )
 }
