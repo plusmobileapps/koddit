91bfafe925f033d54b998d497b7f85e73def1d6f
Andrew Steinmetz <andrew@plusmobileapps.com>
2020-06-17 23:44:58 -0700

    implement very basic scrolling list through the feed

--- web/src/main/kotlin/Home.kt
+++ web/src/main/kotlin/Home.kt
@@ -2,19 +2,33 @@
 import com.plusmobileapps.sharedcode.di.Di
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import react.dom.h1
-import react.dom.render
+import react.dom.*
 import kotlin.browser.document
 
 
 fun main() {
     val api = Di.getRedditFeedApi()
     GlobalScope.launch {
-        console.log(api.getDankMemes())
-    }
-    render(document.getElementById("root")) {
-        h1 {
-            +"Hello, React+Kotlin/JS!"
+        val memes = api.getDankMemes()
+        console.log(memes)
+        render(document.getElementById("root")) {
+            h1 {
+                +"Koddit"
+            }
+
+            div {
+                memes.data.children.forEach { meme ->
+                    p {
+                        +meme.data.title
+                    }
+                    img {
+                        attrs {
+                            src = meme.data.url
+                        }
+                    }
+                }
+            }
         }
     }
+
 }
